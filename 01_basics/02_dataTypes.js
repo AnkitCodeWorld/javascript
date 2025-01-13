8828d39b82462b5027cceb1aead7ea96534ff3f1
AnkitCodeWorld <me.ankit.ar@gmail.com>
2025-01-13 20:29:48 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+//alert(3+3) //we are using nodejs, not browser
+
+console.log(3
+    +3) //code readability should be high
+
+console.log("Ankit")
+
+let name = "Ankit"
+let age = 18
+let isLoggedIn = false
+
+
+// number => 2 t0 power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
